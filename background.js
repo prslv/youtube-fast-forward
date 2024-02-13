1b4447e4b20e91706656f97ceceddab8e867ead6
prslv <presli75@gmail.com>
2024-02-13 09:34:26 +0200

    Updated link to 'whats new' page to repuddle

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function(details) {
     if (details.reason === "update") {
         // Open the "What's New" page in a new tab
-        chrome.tabs.create({ url: "whatsnew.html" });
+        chrome.tabs.create({ url: "https://repuddle.com/extensions/updates/latest/youtube-fast-forward" });
     }
 });
